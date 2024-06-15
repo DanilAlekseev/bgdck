09885c34bd1402fff3655c2d95c96533ef68b376
DanilAlekseev <danadavinci33@gmail.com>
2024-06-15 16:03:55 +0300

    Anecdot Bot

--- src/main/java/ru/DanilAlekseev/project/JokeBot/controller/ClientsController.java
+++ src/main/java/ru/DanilAlekseev/project/JokeBot/controller/ClientsController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/clients")
+@RequestMapping("/clientss")
 public class ClientsController {
     private final ClientsService clientsService;
 
